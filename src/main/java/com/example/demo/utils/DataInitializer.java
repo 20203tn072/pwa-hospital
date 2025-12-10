97d0c41cdf9038b81f7278e9461d96a87cf2188f
Abraham <20203tn072@utez.edu.mx>
2025-12-10 01:41:05 -0600

    Backend actualizado

---
+++ src/main/java/com/example/demo/utils/DataInitializer.java
@@ -0,0 +1,34 @@
+package com.example.demo.utils;
+
+
+import com.example.demo.User.Entity.User;
+import com.example.demo.User.Service.Repository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initDatabase(Repository repository) {
+        return args -> {
+            // Verificar si existe el usuario 'isla'
+            User existingUser = repository.findByUsername("isla");
+
+            if (existingUser == null) {
+                User newUser = new User();
+                newUser.setUsername("island");
+                // Contraseña "12345" encriptada con BCrypt
+                newUser.setPassword("123");
+                newUser.setRole("island");
+                newUser.setNeedAuthentication(false); // O true, según prefieras, pero dijiste 0
+
+                repository.save(newUser);
+                System.out.println(">> USUARIO 'isla' CREADO AUTOMÁTICAMENTE");
+            } else {
+                System.out.println(">> USUARIO 'isla' YA EXISTE. NO SE HICIERON CAMBIOS.");
+            }
+        };
+    }
+}
